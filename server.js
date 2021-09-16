6e33fb2199f2234ea0d5735fc69e1c2cd4af1c6e
ympark99 <ympark1230@naver.com>
2021-09-16 21:25:03 +0900

    dotenv로 환경변수 관리, mongodb 연결

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.listen(8080, function(){
-    console.log('listening on 8080');
-});
+require("dotenv").config();
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://' + process.env.id +':' + process.env.password + '@cluster0.x4v7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err, client){
+    app.listen(8080, function(){
+        console.log('listening on 8080');
+    });
+})
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
