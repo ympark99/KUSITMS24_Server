6f5deae58b196ae3e632b8127daaddb54ce5a7ce
ympark99 <ympark1230@naver.com>
2021-09-16 20:28:48 +0900

    서버 post 요청

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+//body parser 설정
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
     console.log('listening on 8080');
@@ -11,4 +14,13 @@ app.get('/', function(req, res){
 
 app.get('/write', function(req, res){
     res.sendFile(__dirname + '/write.html');
+});
+
+app.get('/beta', function(req, res){
+    res.send('베타 페이지입니다.');
+});
+
+// data 저장
+app.post('/add', function(req, res){
+    res.send(req.body);
 });
